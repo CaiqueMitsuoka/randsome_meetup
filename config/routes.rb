7cc9a3a67d1c5efffb3a6c22b42094ea93c04602
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-01-31 17:30:36 -0200

    Add meetup API

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'meetup#home'
+  get '/auth/meetup/callback', to: 'meetup#callback'
+  get '/events', to: 'meetup#events'
+  get '/events/:urlname/:event_id', to: 'meetup#event'
+  get '/events/:urlname/:event_id/random', to: 'meetup#people_random'
 end
