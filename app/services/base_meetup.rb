906891f6a166357b22a7892f19e7f689640b07f7
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-01-31 17:30:36 -0200

    Add meetup API

---
+++ app/services/base_meetup.rb
@@ -0,0 +1,34 @@
+class BaseMeetup
+  def initialize(token)
+    @token = token
+  end
+
+  def self_events
+    get '/self/events'
+  end
+
+  def attendence(urlname, event_id)
+    get("/#{urlname}/events/#{event_id}/attendance")
+  end
+
+  def event(urlname, event_id)
+    get("/#{urlname}/events/#{event_id}")
+  end
+
+  def get(endpoint, headers = header)
+    parse_json( RestClient.get(meetup_url(endpoint), headers) )
+  end
+
+  private
+  def meetup_url(endpoint = '')
+    'https://api.meetup.com' + endpoint
+  end
+
+  def header
+    { Authorization: "Bearer #{@token}" }
+  end
+
+  def parse_json(response)
+    JSON.parse(response.to_s)
+  end
+end
