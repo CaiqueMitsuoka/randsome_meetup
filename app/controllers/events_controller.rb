3cc9e48f89911be97a073ba50f28410bb0cbc943
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-02-01 13:04:25 -0200

    Refactor meetup controller

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,27 @@
+class EventsController < ApplicationController
+  def index
+    @events = meetup.self_events
+  end
+
+  def show
+    @event = meetup.event(urlname, event_id)
+  end
+
+  def random_attendee
+    attendes = meetup.attendence(urlname, event_id)
+    @person = attendes.shuffle.pop['member']
+  end
+
+  private
+  def meetup
+    BaseMeetup.new session['auth']['credentials']['token']
+  end
+
+  def urlname
+    params.require(:urlname)
+  end
+
+  def event_id
+    params.require(:event_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: 'meetup#home'
   get '/auth/meetup/callback', to: 'meetup#callback'
-  get '/events', to: 'meetup#events'
-  get '/events/:urlname/:event_id', to: 'meetup#event'
-  get '/events/:urlname/:event_id/random', to: 'meetup#people_random'
+  get '/events', to: 'events#index'
+  get '/events/:urlname/:event_id', to: 'events#show'
+  get '/events/:urlname/:event_id/random', to: 'events#random_attendee'
 end
