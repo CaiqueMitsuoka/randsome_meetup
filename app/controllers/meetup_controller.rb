47ef83f6776dbdfbc1cb212f5635e1a32356972c
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-02-02 17:22:36 -0200

    Add auto-refresh for expired token

--- app/controllers/meetup_controller.rb
+++ app/controllers/meetup_controller.rb
@@ -3,7 +3,9 @@ class MeetupController < ApplicationController
   end
 
   def callback
-    session[:auth] = request.env['omniauth.auth']
+    session[:auth] = request.env['omniauth.auth'].deep_symbolize_keys
+    session[:token] = session[:auth][:credentials][:token]
+    session[:refresh_token] = session[:auth][:credentials][:refresh_token]
     redirect_to '/events'
   end
 end
--- app/services/base_meetup.rb
+++ app/services/base_meetup.rb
@@ -1,6 +1,8 @@
 class BaseMeetup
-  def initialize(token)
-    @token = token
+  def initialize(session)
+    @session = session
+    @token = session[:token]
+    @refresh_token = session[:refresh_token]
   end
 
   def self_events(desc = true)
@@ -15,8 +17,30 @@ class BaseMeetup
     parse_json( get("/#{urlname}/events/#{event_id}") )
   end
 
-  def get(endpoint, headers = header)
-    RestClient.get(meetup_url(endpoint), headers)
+  def get(endpoint)
+    atempts ||= 0
+    begin
+      RestClient.get(meetup_url(endpoint), header)
+    rescue RestClient::Unauthorized
+      if !(atempts > 0)
+        atempts += 1
+        puts "Trying to refresh the session token..."
+        refresh_session_token
+        retry
+      end
+    end
+  end
+
+  def refresh_session_token
+    credentials =
+    parse_json( RestClient.post('https://secure.meetup.com/oauth2/access?' +
+    "client_id=#{ENV['MEETUP_KEY']}" +
+    "&client_secret=#{ENV['MEETUP_SECRET']}" +
+    '&grant_type=refresh_token' +
+    "&refresh_token=#{@refresh_token}", nil) )
+
+    @token = @session[:token] = credentials[:access_token]
+    @refresh_token = @session[:refresh_token] = credentials[:refresh_token]
   end
 
   private
