61a008ac873d658d72a02200cb748d3f82c6cc68
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-02-13 20:40:23 -0200

    Add Meetup service test

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -37,55 +37,36 @@ RSpec.describe EventsController, type: :controller do
     allow_any_instance_of(EventsController).to receive(:randsome).and_return(randsome)
     allow_any_instance_of(Randsome).to receive(:meetup).and_return(base_meetup)
   end
-
+  
   describe 'GET #index' do
-    before do
-      base_meetup = double('BaseMeetup', self_events: [])
-      allow_any_instance_of(EventsController).to receive(:meetup).and_return(base_meetup)
-    end
-
     it 'assign events' do
       get :index
       expect(assigns(:events)).to match_array([])
     end
+
+    it 'call the randsome service' do
+      expect(randsome).to receive(:available_events)
+      get :index
+    end
   end
 
   describe 'GET #show' do
-    before do
-      @base_meetup = double('BaseMeetup', event: {})
-      allow_any_instance_of(EventsController).to receive(:meetup).and_return(@base_meetup)
-    end
-
     it 'assign event' do
       get_event
       expect(assigns(:event)).to match_array({})
     end
 
     it 'call event of base meetup' do
-      expect(@base_meetup).to receive(:event).with('guru-sorocaba','1b3b4fe')
+      expect(base_meetup).to receive(:event).with('guru-sorocaba','1b3b4fe')
       get_event
     end
 
     def get_event
-      get :show, params: { urlname: 'guru-sorocaba', event_id: '1b3b4fe' }
+      get :show, params: event_payload
     end
   end
 
   describe 'GET #random_attendee' do
-    let(:name) { 'Mary' }
-    let(:img_name) { 'image.jpg' }
-
-    before do
-      person = {
-        name: name,
-        photo: {
-          thumb: img_name
-        }
-      }
-      @base_meetup = double('BaseMeetup', attendance: [{ member: person }])
-      allow_any_instance_of(EventsController).to receive(:meetup).and_return(@base_meetup)
-    end
-
     it 'assign image' do
       get_random_attendee
       expect(assigns(:image)).to eq(img_name)
@@ -96,13 +77,13 @@ RSpec.describe EventsController, type: :controller do
       expect(assigns(:name)).to eq(name)
     end
 
-    it 'call attendence of base meetup' do
-      expect(@base_meetup).to receive(:attendance).with('guru-sorocaba','1b3b4fe')
+    it 'call attendence of randsome' do
+      expect(randsome).to receive(:random_attendee).with('guru-sorocaba','1b3b4fe')
       get_random_attendee
     end
 
     def get_random_attendee
-      get :random_attendee, params: { urlname: 'guru-sorocaba', event_id: '1b3b4fe' }
+      get :random_attendee, params: event_payload
     end
   end
 end
